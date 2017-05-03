9c08bae7363dc5834e1f2e2e0a180d3e4fafde08
user <user@192.168.56.1>
2017-05-03 07:50:58 +0700

    Finish

---
+++ src/contohprojekmvc/ContohProjekMVC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contohprojekmvc;
+
+/**
+ *
+ * @author user
+ */
+public class ContohProjekMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
